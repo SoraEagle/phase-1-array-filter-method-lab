081cced6527597d7d0082a41f681ba721c4dc23a
Matthew <phillips.matthew829@gmail.com>
2021-11-09 15:33:23 -0600

    commit tree 97219fb856e977c5e92a57205f12cb3a7ed2084d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(driver => 
+        driver.toLowerCase() === string.toLowerCase())
+}
+
+let fuzzyMatch = (drivers, letter) => {
+    return drivers.filter(driver => driver.startsWith(letter))
+  }
+
+function matchName(drivers, string){
+    return drivers.filter(driver => driver.name === string)
+}
